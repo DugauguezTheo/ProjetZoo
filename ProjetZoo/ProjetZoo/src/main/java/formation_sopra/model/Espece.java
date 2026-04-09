036618042752980ba9c41595a09d29c64eda69f2
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-04-09 18:08:12 +0200

    Add Panda_Roux (le plus bel animal au monde)

--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/model/Espece.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/model/Espece.java
@@ -2,7 +2,7 @@ package formation_sopra.model;
 
 public enum Espece {
 
-    Lion,Tigre,Elephant,Rhinoceros,Hippopotame,Girafe,Zebre,Panthere,Leopard,Guepard,Ours_Polaire,Ours_Brun,Panda,
+    Lion,Tigre,Elephant,Rhinoceros,Hippopotame,Girafe,Zebre,Panthere,Leopard,Guepard,Ours_Polaire,Ours_Brun,Panda,Panda_Roux,
     Koala,Kangourou,Orang_Outan,Gorille,Chimpanze,Bonobo,Lemurien,Loup,Loup_Artique,Hyene,Jaguar,Couguar,Lynx,Manchot,
     Autruche,Flamant_Rose,Perroquet,Toucan,Aigle,Faucon,Chouette,Crocodile,Alligator,Serpent_Python,Cobra,Varan,
     Tortue_Geante,Dauphin,Otarie,Phoque,Manta,Requin,Pingouin,Cameleon,Iguane,Suricate;
