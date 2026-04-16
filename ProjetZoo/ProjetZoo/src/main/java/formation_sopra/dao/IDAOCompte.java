8393c03236bc0052176f38f01ca56de2d55f95f6
Matth-Cbn <matthieu.corbin@outlook.fr>
2026-04-16 19:17:00 +0200

    Import correction IDAO Compte/Visiteur

--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOCompte.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOCompte.java
@@ -2,7 +2,7 @@ package formation_sopra.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Compte;
+import formation_sopra.model.Compte;
 
 public interface IDAOCompte extends JpaRepository<Compte, Integer> {
 
--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOVisiteur.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOVisiteur.java
@@ -2,7 +2,7 @@ package formation_sopra.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Visiteur;
+import formation_sopra.model.Visiteur;
 
 public interface IDAOVisiteur extends JpaRepository<Visiteur, Integer> {
 
