27c8878e3ccf022a8266107a0b368e00f6415eb7
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-06 10:57:00 +0200

    Verif Spectacle ok

--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/SpectacleController.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/SpectacleController.java
@@ -3,11 +3,11 @@ package formation_sopra.controller;
 import formation_sopra.controller.dto.request.CreateOrUpdateSpectacleRequest;
 import formation_sopra.controller.dto.response.EntityCreatedOrUpdatedResponse;
 import formation_sopra.controller.dto.response.SpectacleResponse;
+import formation_sopra.controller.dto.response.SpectacleWithReservationsResponse;
 import formation_sopra.dao.IDAOEnclos;
 import formation_sopra.dao.IDAOReservation;
 import formation_sopra.dao.IDAOSpectacle;
 import formation_sopra.model.Enclos;
-import formation_sopra.model.Reservation;
 import formation_sopra.model.Spectacle;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
@@ -18,11 +18,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/spectacle")
-@PreAuthorize("hasRole('ADMIN')") // Still need to decide who can access
 public class SpectacleController {
 
     private static Logger log = LoggerFactory.getLogger(SpectacleController.class);
@@ -39,32 +39,56 @@ public class SpectacleController {
 
 
 
+    @GetMapping("/{id}/with-reservations")
+    @PreAuthorize("hasRole('ADMIN')")
+    public SpectacleWithReservationsResponse findByIdWithReservations(@PathVariable Integer id) {
+        
+        log.debug("Spectacle {} avec les réservations ...", id);
+
+        return daoSpectacle.findByIdWithReservations(id).map(SpectacleWithReservationsResponse::convert).orElse(null);    
+    }
+
     @GetMapping("/{id}")
     public SpectacleResponse findById(@PathVariable Integer id) {
         
         log.debug("Spectacle {} ...", id);
 
-        // return daoSpectacle.findById(id).map(SpectacleResponse::convert).orElse(null);  
-        return daoSpectacle.findByIdWithReservation(id).map(SpectacleResponse::convert).orElse(null);    
+        return daoSpectacle.findById(id).map(SpectacleResponse::convert).orElse(null);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<SpectacleResponse> findAll() {
         log.debug("Liste des spectacles ...");
 
-        // return this.daoSpectacle.findAll().stream().map(SpectacleResponse::convert).toList();
-        return this.daoSpectacle.findAllWithReservation().stream().map(SpectacleResponse::convert).toList();
+        return this.daoSpectacle.findAllWithReservations().stream().map(SpectacleResponse::convert).toList();
+    }
+
+    @GetMapping("/as-visiteur")
+    public List<SpectacleResponse> findAllBetween(@RequestParam LocalDate dateDebut, @RequestParam LocalDate dateFin) {
+        
+        log.debug("Liste des spectacles entre le {} et le {}", dateDebut, dateFin);
+
+        return daoSpectacle.findAllBetween(dateDebut, dateFin).stream().map(SpectacleResponse::convert).toList();
     }
 
     @GetMapping("/enclos/{id}")
-    public List<SpectacleResponse> findAllByEnclosId(@PathVariable Integer id) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<SpectacleWithReservationsResponse> findAllByEnclosId(@PathVariable Integer id) {
         log.debug("Liste des spectacles pour l'enclos {}...", id);
         Enclos e = this.daoEnclos.findById(id).orElseThrow(EntityNotFoundException::new);
-        return this.daoSpectacle.findAllByEnclosWithReservation(e).stream().map(SpectacleResponse::convert).toList();
+        return this.daoSpectacle.findAllByEnclosWithReservations(e).stream().map(SpectacleWithReservationsResponse::convert).toList();
+    }
+
+    @GetMapping("/enclos/{id}/as-visiteur")
+    public List<SpectacleResponse> findAllByEnclosIdBetween(@PathVariable Integer id, @RequestParam LocalDate dateDebut, @RequestParam LocalDate dateFin) {
+        log.debug("Liste des spectacles pour l'enclos {} entre le {} et le {}...", id, dateDebut, dateFin);
+        return this.daoSpectacle.findAllByEnclosIdBetween(id, dateDebut, dateFin).stream().map(SpectacleResponse::convert).toList();
     }
     
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public EntityCreatedOrUpdatedResponse update(@PathVariable Integer id, @Valid @RequestBody CreateOrUpdateSpectacleRequest request) {
         log.debug("Modification du spectacle {} ...", id);
 
@@ -75,21 +99,7 @@ public class SpectacleController {
         spectacle.setHeureDebut(request.getHeureDebut());
         spectacle.setDuree(request.getDuree()); 
         spectacle.setEnclos(this.daoEnclos.findById(request.getEnclosId()).orElseThrow(EntityNotFoundException::new));
-    
-    //Old version
-    //     if (request.getReservationIds() != null) {
-    //         List<Reservation> reservations = daoReservation.findAllById(request.getReservationIds());
-    //         spectacle.setReservations(reservations);
-
-    //         spectacle.setReservations(reservations);
-
-    //         for (Reservation r : reservations) {
-    //             r.getSpectacles().add(spectacle);
-    //         }
-    // }
-
-    //New version -> spectacle mappedBy reservation
-        // spectacle.setReservations(this.daoReservation.findAllById(request.getReservationIds()));
+        spectacle.setReservations(this.daoReservation.findAllBySpectacleId(id));
 
         this.daoSpectacle.save(spectacle);
 
@@ -100,7 +110,7 @@ public class SpectacleController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public EntityCreatedOrUpdatedResponse create(@RequestBody CreateOrUpdateSpectacleRequest request) {
+    public EntityCreatedOrUpdatedResponse create(@Valid @RequestBody CreateOrUpdateSpectacleRequest request) {
         log.debug("Création d'un nouveau spectacle ...");
 
         Spectacle spectacle = new Spectacle();
@@ -109,21 +119,6 @@ public class SpectacleController {
         spectacle.setHeureDebut(request.getHeureDebut());
         spectacle.setDuree(request.getDuree());
         spectacle.setEnclos(this.daoEnclos.findById(request.getEnclosId()).orElseThrow(EntityNotFoundException::new));
-    
-    //Old version
-    //     if (request.getReservationIds() != null) {
-    //         List<Reservation> reservations = daoReservation.findAllById(request.getReservationIds());
-    //         spectacle.setReservations(reservations);
-
-    //         spectacle.setReservations(reservations);
-
-    //         for (Reservation r : reservations) {
-    //             r.getSpectacles().add(spectacle);
-    //         }
-    // }
-
-    //New version -> spectacle mappedBy reservation
-        // spectacle.setReservations(this.daoReservation.findAllById(request.getReservationIds()));
 
         this.daoSpectacle.save(spectacle);
 
--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOSpectacle.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/dao/IDAOSpectacle.java
@@ -1,5 +1,6 @@
 package formation_sopra.dao;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,15 +14,20 @@ import formation_sopra.model.Spectacle;
 public interface IDAOSpectacle extends JpaRepository<Spectacle,Integer> {
 
     @Query("SELECT DISTINCT s FROM Spectacle s LEFT JOIN FETCH s.reservations")
-    List<Spectacle> findAllWithReservation();
+    List<Spectacle> findAllWithReservations();
         
     @Query("SELECT DISTINCT s FROM Spectacle s LEFT JOIN FETCH s.reservations")
-    Optional<Spectacle> findByIdWithReservation(@Param("id") Integer id);
+    Optional<Spectacle> findByIdWithReservations(@Param("id") Integer id);
 
     @Query("SELECT DISTINCT s FROM Spectacle s LEFT JOIN FETCH s.reservations WHERE s.enclos = :enclos")
-    List<Spectacle> findAllByEnclosWithReservation(@Param("enclos") Enclos enclos);
+    List<Spectacle> findAllByEnclosWithReservations(@Param("enclos") Enclos enclos);
 
     @Query("SELECT s FROM Spectacle s WHERE s.enclos.numero = :id")
     List<Spectacle> findAllByEnclosId(@Param("id") Integer id);
     
+    @Query("SELECT s FROM Spectacle s WHERE s.dateDebut BETWEEN :debut AND :fin")
+    List<Spectacle> findAllBetween(@Param("debut") LocalDate dateDebut, @Param("fin") LocalDate dateFin);
+
+    @Query("SELECT s FROM Spectacle s WHERE s.id = :id AND s.dateDebut BETWEEN :debut AND :fin")
+    List<Spectacle> findAllByEnclosIdBetween(@Param("id") Integer id, @Param("debut") LocalDate dateDebut, @Param("fin") LocalDate dateFin);
 }
