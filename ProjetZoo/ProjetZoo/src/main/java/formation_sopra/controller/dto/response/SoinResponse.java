8c7e6f3f4458bd700cd46689a20af8ba67ff8857
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-06 12:38:05 +0200

    Soin : added descritption in response

--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/dto/response/SoinResponse.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/dto/response/SoinResponse.java
@@ -5,12 +5,13 @@ import java.time.LocalDateTime;
 import formation_sopra.model.Soin;
 
 public record SoinResponse(
-    Integer id, LocalDateTime dateSoin, Integer veterinaireId, Integer animalId
+    Integer id, LocalDateTime dateSoin, String descritpion, Integer veterinaireId, Integer animalId
 ) {
     public static SoinResponse convert(Soin soin) {
         SoinResponse resp = new SoinResponse(
             soin.getId(), 
             soin.getDateSoin(),
+            soin.getDescription(),
             soin.getVeterinaire().getId(), 
             soin.getAnimal().getId()
         );
