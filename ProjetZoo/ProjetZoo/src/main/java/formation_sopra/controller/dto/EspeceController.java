12f3d11920470492199469a24f47deb067dd36b3
Lauriane Bruna <brn.lauriane@gmail.com>
2026-05-06 15:22:35 +0200

    EspeceController

---
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/dto/EspeceController.java
@@ -0,0 +1,22 @@
+package formation_sopra.controller.dto;
+
+import formation_sopra.model.Espece;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/espece")
+public class EspeceController {
+
+    @GetMapping
+    public List<String> getEspeces() {
+        return Arrays.stream(Espece.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+}
