639713892938003dd8d28ab79c71454eaeafd4ea
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-06 15:14:10 +0200

    Add AuthGuard, AuthResponse, AuthRequest

---
+++ ProjetZoo_angular/projet_zoo_angular/src/app/guard/auth-guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../service/auth-service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  if (authService.isLogged()) {
+    return true;
+  }
+
+  return router.createUrlTree([ '/login' ]);
+};
