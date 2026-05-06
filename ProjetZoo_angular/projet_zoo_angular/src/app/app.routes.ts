e83a84cd0fed56ab0bdd519be99f3df23bc03b5b
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-06 14:50:39 +0200

    Base app_routes

--- ProjetZoo_angular/projet_zoo_angular/src/app/app.routes.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'home', component: AccueilPage, canActivate: [authGuard] },
+    { path: 'livre', component: Livres, canActivate: [authGuard] },
+    { path: 'auteur', component: AuteurPage, canActivate: [authGuard] },
+    { path: 'collection', component: CollectionsPage, canActivate: [authGuard] },
+    { path: 'editeur', component: Editeurs, canActivate: [authGuard] },
+    { path: 'avis', component: AvisPage, canActivate: [authGuard] },
+
+
+    // { path: 'home', component: AccueilPage },
+    // { path: 'livre', component: Livres },
+    // { path: 'auteur', component: AuteurPage },
+    // { path: 'collection', component: CollectionsPage },
+    // { path: 'editeur', component: Editeurs },
+    // { path: 'avis', component: AvisPage },
+
+    { path: 'login', component: LoginPage },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
