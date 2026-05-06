66ddde15c852dd2cc1151e566b464cd9fcc3b661
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-06 16:42:45 +0200

    Add Connexion (côté TS)

--- ProjetZoo_angular/projet_zoo_angular/src/app/component/connexion/connexion.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/component/connexion/connexion.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth-service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthRequest } from '../../dto/auth-request';
 
 @Component({
   selector: 'app-connexion',
@@ -6,4 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './connexion.html',
   styleUrl: './connexion.css',
 })
-export class Connexion {}
+export class Connexion implements OnInit{
+
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+  private formBuilder: FormBuilder = inject(FormBuilder);
+
+  protected formAuth!: FormGroup;
+  protected formUsernameCtrl!: FormControl;
+  protected formPasswordCtrl!: FormControl;
+
+  ngOnInit(): void {
+
+    this.formUsernameCtrl = this.formBuilder.control("", [ Validators.required, Validators.email ]);
+    this.formPasswordCtrl = this.formBuilder.control("", Validators.required);
+
+    this.formAuth = this.formBuilder.group({
+      username: this.formUsernameCtrl,
+      password: this.formPasswordCtrl
+    });
+  }
+
+  public connexion() {
+    const authRequest: AuthRequest = {
+      username: this.formUsernameCtrl.value,
+      password: this.formPasswordCtrl.value
+    };
+
+    this.authService.auth(authRequest).subscribe(resp => {
+      if (resp.success) {
+        this.authService.token = resp.token;
+        this.router.navigate([ '/animal' ]);
+      }
+    });
+  }
+}
