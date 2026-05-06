5285751055a4576d5ebe152d7a081677653e221b
Lauriane Bruna <brn.lauriane@gmail.com>
2026-05-06 11:59:43 +0200

    Component Connexion + gif panthere

--- ProjetZoo_angular/projet_zoo_angular/src/app/app.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Connexion } from "./component/connexion/connexion";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Connexion],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('projet_zoo_angular');
 }
