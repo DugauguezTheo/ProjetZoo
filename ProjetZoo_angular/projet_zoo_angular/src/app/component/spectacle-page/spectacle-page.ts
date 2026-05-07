f38c21d8500bcc30cd0c0a5a5e0d3139046cd001
Jean-Guy WINTREBERT <jg.wintrebert@orange.fr>
2026-05-07 18:41:53 +0200

    Spectacle V1 : front (html, ts, css & service)

--- ProjetZoo_angular/projet_zoo_angular/src/app/component/spectacle-page/spectacle-page.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/component/spectacle-page/spectacle-page.ts
@@ -1,9 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Spectacle } from '../../model/spectacle';
+import { SpectacleService } from '../../service/spectacle-service';
+import { Observable, startWith, Subject, switchMap } from 'rxjs';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Enclos } from '../../model/enclos';
+import { EnclosService } from '../../service/enclos-service';
+import { SpectacleRequest } from '../../dto/spectacle-request';
 
 @Component({
   selector: 'app-spectacle-page',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './spectacle-page.html',
   styleUrl: './spectacle-page.css',
 })
-export class SpectaclePage {}
+export class SpectaclePage {
+
+
+  private titleService: Title = inject(Title);
+  private router: Router = inject(Router);
+
+  private spectacleService: SpectacleService = inject(SpectacleService);
+  private enclosService: EnclosService = inject(EnclosService);
+  // private reservationService: ReservationService = inject(ReservationService);
+
+  protected editingSpectacle?: Spectacle | null;
+  protected spectacles$!: Observable<Spectacle[]>;
+
+  protected enclos!: Observable<Enclos[]>;
+
+  // protected reservations$!: Observable<Reservation[]>;
+
+  private refresh$: Subject<void> = new Subject<void>();
+
+  private formBuilder: FormBuilder = inject(FormBuilder);
+
+  protected formSpectacle!: FormGroup;
+  
+  protected formDateCtrl!: FormControl;
+  protected formHeureCtrl!: FormControl;
+  protected formDureeCtrl!: FormControl;
+  protected formEnclosCtrl!: FormControl;
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Zoo AJC - Spectacle');
+    this.enclos = this.enclosService.findAllEncloss();
+
+    // this.reservations$ = this.reservationService.findAllEspeces();
+
+    this.spectacles$ = this.refresh$.pipe(
+      startWith(0),
+      switchMap(() => this.spectacleService.findAllSpectacles())
+    );
+
+    this.formDateCtrl = this.formBuilder.control('', Validators.required);
+    this.formHeureCtrl = this.formBuilder.control('', Validators.required);
+    this.formDureeCtrl = this.formBuilder.control('', Validators.required);
+    this.formEnclosCtrl = this.formBuilder.control('', Validators.required);
+
+    this.formSpectacle = this.formBuilder.group({
+      date: this.formDateCtrl,
+      heure: this.formHeureCtrl,
+      duree: this.formDureeCtrl,
+      enclos: this.formEnclosCtrl
+    });
+  }
+
+  private reload() {
+    this.refresh$.next();
+  }
+
+  public addOrUpdateSpectacle() {
+    const spectacle: SpectacleRequest = {
+      dateDebut: this.formDateCtrl.value,
+      heureDebut: this.formHeureCtrl.value,
+      duree: this.formDureeCtrl.value,
+      enclosId: this.formEnclosCtrl.value
+    };
+    
+    if (this.editingSpectacle) {
+      this.spectacleService.updateSpectacle(this.editingSpectacle.id, spectacle).subscribe(() => {
+        this.editingSpectacle = null;
+        this.formSpectacle.reset();
+        this.reload();
+      });
+    }
+    else {
+      this.spectacleService.addSpectacle(spectacle).subscribe(() => {
+        this.formSpectacle.reset();
+        this.reload();
+      })
+    }
+  }
+
+  public editerSpectacle(spectacle: Spectacle) {
+    this.editingSpectacle = spectacle;
+
+    this.formDateCtrl.setValue(spectacle.dateDebut);
+    this.formHeureCtrl.setValue(spectacle.heureDebut);
+    this.formDureeCtrl.setValue(spectacle.duree);
+    this.formEnclosCtrl.setValue(spectacle.enclosId);
+
+    this.reload();
+  }
+
+  // public compareEnclos(e1: Enclos, e2: Enclos): boolean {
+  //   return e1 && e2 ? e1.numero === e2.numero : e1 === e2;
+  // }
+
+  public deleteSpectacle(spectacle: Spectacle) {
+    this.spectacleService.deleteSpectacle(spectacle.id).subscribe(() => {
+      this.reload();
+    });
+  }
+
+  // public chargerListeEspeces(numero: number): void {
+  //   this.especes$ = this.spectacleService.getAllEspecesInSpectacle(numero).pipe(
+  //     map(spectacle => (spectacle.especes ?? []) as string[])
+  //   );
+  // }
+
+
+
+}
