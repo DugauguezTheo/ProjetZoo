aaeed9002b9a85676b7a181d6acb2c161f1b4cf0
Matth-Cbn <matthieu.corbin@outlook.fr>
2026-05-07 16:25:16 +0200

    Page admin : Ajout LogViewer + retrait lien navigation manuel

--- ProjetZoo_angular/projet_zoo_angular/src/app/component/admin-page/admin-page.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/component/admin-page/admin-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogViewer } from '../log-viewer/log-viewer';
 
 @Component({
   selector: 'app-admin-page',
-  imports: [],
+  imports: [LogViewer],
   templateUrl: './admin-page.html',
   styleUrl: './admin-page.css',
 })
