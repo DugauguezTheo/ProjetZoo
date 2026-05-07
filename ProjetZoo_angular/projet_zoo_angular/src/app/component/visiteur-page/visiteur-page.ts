037d838c1700bb435f12bc144078c0e4c3fb8eb4
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-07 10:42:53 +0200

    Page Visiteur connecté
    Il faudra intégrer le menu de Tanguy et binder les données

--- ProjetZoo_angular/projet_zoo_angular/src/app/component/visiteur-page/visiteur-page.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/component/visiteur-page/visiteur-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-visiteur-page',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './visiteur-page.html',
   styleUrl: './visiteur-page.css',
 })
