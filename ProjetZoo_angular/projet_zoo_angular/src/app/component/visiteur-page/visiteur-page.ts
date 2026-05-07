28dc2e73d698ff21bda8b8cdb7cae0895eb863c2
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-07 11:10:33 +0200

    Page Visiteur : ajout titre et centrage du contenu principal

--- ProjetZoo_angular/projet_zoo_angular/src/app/component/visiteur-page/visiteur-page.ts
+++ ProjetZoo_angular/projet_zoo_angular/src/app/component/visiteur-page/visiteur-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../service/auth-service';
 
 @Component({
   selector: 'app-visiteur-page',
@@ -9,4 +11,12 @@ import { RouterModule } from '@angular/router';
   templateUrl: './visiteur-page.html',
   styleUrl: './visiteur-page.css',
 })
-export class VisiteurPage {}
+export class VisiteurPage implements OnInit {
+  private titleService: Title = inject(Title);
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Welcome "visiteur.prenom" - AJC-Zoo');
+  }
+}
