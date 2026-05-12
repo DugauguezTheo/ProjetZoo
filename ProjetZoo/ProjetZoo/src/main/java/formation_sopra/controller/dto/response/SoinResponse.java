489bd49896c232ea04d8cb03e9657c808eb20b24
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-12 10:56:26 +0200

    Soin : Add description

--- ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/dto/response/SoinResponse.java
+++ ProjetZoo/ProjetZoo/src/main/java/formation_sopra/controller/dto/response/SoinResponse.java
@@ -7,7 +7,7 @@ import formation_sopra.model.Soin;
 
 
 public record SoinResponse(
-    Integer id, LocalDateTime dateSoin, String descritpion, VeterinaireResponse veterinaire, AnimalResponse animal
+    Integer id, LocalDateTime dateSoin, String description, VeterinaireResponse veterinaire, AnimalResponse animal
 ) {
     public static SoinResponse convert(Soin soin) {
         SoinResponse resp = new SoinResponse(
